463105105311e0adc5ce2c458c57a3a2beffa11e
Michael Dähnert <1017301+mdaehnert@users.noreply.github.com>
2019-05-01 15:49:51 +0200

    Adding S3 access.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { S3 } from "aws-sdk";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cloud-photo-safari-aws';
+
+  constructor() {
+    console.log(123);
+  }
+
+  public ngOnInit() {
+    var x = new S3().getSignedUrl('GetObject', {Bucket: 'foo', Key: 'bar'});
+    console.log(x);
+  }
 }
